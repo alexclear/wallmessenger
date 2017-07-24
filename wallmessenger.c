106d5131d399724c1c9051b2d0f5bf5961238c94
Alex Chistyakov <alexclear@gmail.com>
2017-07-24 15:17:55 +0300

    Parse a YAML config

--- wallmessenger.c
+++ wallmessenger.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <yaml.h>
+#include <string.h>
 /*
 //#define TRUE FALSE // Счастливой отладки!
 */
+
+#define PORT_NUMBER "port_number"
+#define USER_NAME "user_name"
 
 typedef struct {
     char* user_name;
@@ -15,7 +19,7 @@ typedef struct {
 config_t config;
 
 int main(int argc, char* argv[]) {
-    int i=0;
+    //int i=0;
     int c;
 
 /*
@@ -64,6 +68,8 @@ int main(int argc, char* argv[]) {
     yaml_event_t event;
 
     int done=0;
+    int nextval_port_number=0;
+    int nextval_user_name=0;
 
     /* Read the event sequence. */
     while (!done) {
@@ -74,11 +80,29 @@ int main(int argc, char* argv[]) {
            exit(EXIT_FAILURE);
         }
 
-        /*
-          ...
-          Process the event.
-          ...
-        */
+        if(event.type == YAML_SCALAR_EVENT) {
+            if(event.data.scalar.value != NULL) {
+                fprintf(stderr, "value: %s\n", event.data.scalar.value);
+                if(nextval_port_number == 1) {
+                    config.port_number = malloc(event.data.scalar.length + 1);
+                    strncpy(config.port_number, event.data.scalar.value, event.data.scalar.length);
+                    config.port_number[event.data.scalar.length] = NULL;
+                    nextval_port_number = 0;
+                }
+                if(strncmp(PORT_NUMBER, event.data.scalar.value, event.data.scalar.length) == 0) {
+                    nextval_port_number = 1;
+                }
+                if(nextval_user_name == 1) {
+                    config.user_name = malloc(event.data.scalar.length + 1);
+                    strncpy(config.user_name, event.data.scalar.value, event.data.scalar.length);
+                    config.user_name[event.data.scalar.length] = NULL;
+                    nextval_user_name = 0;
+                }
+                if(strncmp(USER_NAME, event.data.scalar.value, event.data.scalar.length) == 0) {
+                    nextval_user_name = 1;
+                }
+            }
+        } 
 
         /* Are we finished? */
         done = (event.type == YAML_STREAM_END_EVENT);
@@ -95,6 +119,9 @@ int main(int argc, char* argv[]) {
         perror("Can't close the config file");
         exit(EXIT_FAILURE);
     }
+
+    fprintf(stderr, "Port number: %s\n", config.port_number);
+    fprintf(stderr, "User name: %s\n", config.user_name);
 
     exit(EXIT_SUCCESS);
 }
